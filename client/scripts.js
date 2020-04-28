ad951b16ab3f3b87ec5675ae6c6ad4a3449359bb
Benson <bennywhyu@gmail.com>
2020-04-28 04:19:56 -0400

    submit url script and loading creators script

--- client/scripts.js
+++ client/scripts.js
@@ -12,12 +12,35 @@ function newElement(type, attributes = {}) {
     return element;
 }
 
-const ctr = document.querySelector('.container')
-const card = newElement('div', { class: 'card' })
-
-ctr.appendChild(card)
-
 function submitURL() {
     const input = document.querySelector('.url-input').value
 
+    fetch('http://localhost:3000/creators', {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify({ input })
+    })
+
 }
+
+async function loadCreators() {
+    const res = await fetch('http://localhost:3000/creators')
+    const creators = await res.json()
+
+    const ctr = document.querySelector('.container')
+
+    creators.forEach(creator => {
+        const card = newElement('div', { class: 'card' })
+        const title = newElement('h4', { innerText: creator.name })
+        const img = newElement('img', { src: creator.img })
+
+
+        ctr.appendChild(title)
+        ctr.appendChild(img)
+        ctr.appendChild(card)
+    })
+}
+
+loadCreators()
